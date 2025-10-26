e564d36502feb82a56f365b293955551b1f409e8
AN Long <aisk1988@gmail.com>
2025-10-26 23:02:53 +0900

    Add basic Protocol class

--- thriftpy2/protocol/rust_apache_json.rs
+++ thriftpy2/protocol/rust_apache_json.rs
@@ -1,6 +1,41 @@
 use pyo3::prelude::*;
 
+#[pyclass]
+pub struct TApacheJSONProtocol {
+    trans: PyObject,
+    req: Option<String>,
+}
+
+#[pymethods]
+impl TApacheJSONProtocol {
+    #[new]
+    pub fn new(trans: PyObject) -> PyResult<Self> {
+        Ok(TApacheJSONProtocol {
+            trans: trans,
+            req: None,
+        })
+    }
+}
+
+#[pyclass]
+pub struct TApacheJSONProtocolFactory;
+
+#[pymethods]
+impl TApacheJSONProtocolFactory {
+    #[new]
+    fn new() -> Self {
+        TApacheJSONProtocolFactory
+    }
+
+    fn get_protocol(&self, trans: PyObject) -> PyResult<TApacheJSONProtocol> {
+        TApacheJSONProtocol::new(trans)
+    }
+}
+
 #[pymodule]
+#[pyo3(name="rust_apache_json")]
 fn rust_apache_json(m: &Bound<'_, PyModule>) -> PyResult<()> {
+    m.add_class::<TApacheJSONProtocol>()?;
+    m.add_class::<TApacheJSONProtocolFactory>()?;
     Ok(())
 }
